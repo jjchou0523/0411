8f2cd67538039460a76f2854083e52eae1ee619b
CHOU,MEI-LIEN <jumi.8905231119@gmail.com>
2017-03-28 20:09:07 +0800

    Signed-off-by: CHOU,MEI-LIEN <jumi.8905231119@gmail.com>

---
+++ 猜數字/猜數字/猜數字/Form1.vb
@@ -0,0 +1,17 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Randomize()
+        Dim target As Integer = CInt(Int((100 * Rnd()) + 1))
+        Dim guess As Integer
+        Do
+            guess = Val(InputBox("請輸入一數字"))
+            If (target > guess) Then
+                MsgBox("猜大一點")
+            ElseIf (target < guess) Then
+                MsgBox("猜小一點")
+            Else
+                MsgBox("猜中了")
+            End If
+        Loop While (target <> guess)
+    End Sub
+End Class
